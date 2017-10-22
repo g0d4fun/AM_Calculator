e36d12d49cea2a48ecb5b8391b533e740bb89a80
Rafa <henriques.rafa.filipe@gmail.com>
2017-10-22 11:34:40 +0100

    Toast added when click on CalView

--- app/src/main/java/com/example/rafa/calculator_desktop/MainActivity.java
+++ app/src/main/java/com/example/rafa/calculator_desktop/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.rafa.calculator_desktop;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -88,7 +90,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickResult(View v) {
+        if(isNewNumber) return;
 
+        String toCal = calculatorView.getText().toString();
+    }
+
+    public void showToastOnClickCalculatorView(View v){
+        Toast toast = getToast("Calculator made by Rafa.");
+        toast.show();
+    }
+
+    public Toast getToast(String textToPrint){
+        Context context = getApplicationContext();
+        CharSequence text = textToPrint;
+        int duration = Toast.LENGTH_SHORT;
+
+        return Toast.makeText(context, text, duration);
     }
 }
 
